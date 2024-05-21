25c45abce6ffda30c931cf67dcc7d4310b906774
Nicolas Wormser <nicolas@codewithflair.org>
2024-05-21 22:24:42 +0200

    Revert "Merge pull request #5 from WildCodeSchool-2024-02/FixGlobal"
    This reverts commit d3b89ad2110b5e7b68cf0b913e50b1a877dd6d20, reversing
    changes made to f11c467376b3be488cf840aef4e94c8f908f25f1.

--- client/src/components/FavoriteSongs/FavoriteSongs.jsx
+++ client/src/components/FavoriteSongs/FavoriteSongs.jsx
@@ -1,10 +1,1 @@
-import "./FavoriteSongs_Desk.css";
 
-export default function FavoriteSongs() {
-  return (
-    <div className="myfavorite">
-      <h3>Favorites Songs</h3>
-      <hr />
-    </div>
-  );
-}
